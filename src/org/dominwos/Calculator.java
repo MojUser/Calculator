de1b5a71f3ce40d3fbdbf033273a5ee997a3eacc
Dominik.Wosinski <bl@wp.pl>
2019-01-26 14:13:51 +0100

    Power added

--- src/org/dominwos/Calculator.java
+++ src/org/dominwos/Calculator.java
@@ -10,4 +10,8 @@ public class Calculator {
     public int multiply(int a, int b ) {
         return a * b;
     }
+
+    public double power(double a, double b) {
+        return Math.pow(a, b);
+    }
 }
