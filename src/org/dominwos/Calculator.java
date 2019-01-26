2a06f08f81cd5b2c8dabf545809b39743d7187b9
Dominik.Wosinski <bl@wp.pl>
2019-01-26 13:03:57 +0100

    Subtraction addded

--- src/org/dominwos/Calculator.java
+++ src/org/dominwos/Calculator.java
@@ -1,4 +1,13 @@
 package org.dominwos;
 
 public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 }
