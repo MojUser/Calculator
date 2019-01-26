be5b4bba9aca646309c010394bdcb52fab8eaafa
Dominik.Wosinski <bl@wp.pl>
2019-01-26 14:32:22 +0100

    Slight Changes

--- src/org/dominwos/Calculator.java
+++ src/org/dominwos/Calculator.java
@@ -2,15 +2,12 @@ package org.dominwos;
 
 public class Calculator {
 
-    public int add(int a, int b) {
-        return a + b;
-
-    }
-
     public int multiply(int a, int b ) {
         return a * b;
     }
-
+    public double power(double a, double b) {
+        return Math.pow(a, b);
+    }
     public double power(double a, double b) {
         return Math.pow(a, b);
     }
