5d63e6bd7300604a598102f61ac8de4355c54cde
Seba <sebastian.ryczek1@gmail.com>
2019-01-26 15:40:23 +0100

    dodano interfejs

--- src/org/dominwos/Main.java
+++ src/org/dominwos/Main.java
@@ -1,8 +1,50 @@
 package org.dominwos;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        Calculator obj = new Calculator();
+        Main main = new Main();
+
+        System.out.println("Wybierz operacje: 1.+ 2./ 3.* 4.- 5.pow 6. sqrt ");
+        switch (sc.nextInt()) {
+            case 1:
+                System.out.println("Wykonujesz dodawanie");
+                System.out.println(obj.add(main.daneZKlawiatury(), main.daneZKlawiatury()));
+                break;
+            case 2:
+                System.out.println("Wykonujesz dzielenie");
+                System.out.println(obj.divide(main.daneZKlawiatury(), main.daneZKlawiatury()));
+                break;
+            case 3:
+                System.out.println("Wykonujesz mnozenie");
+                System.out.println(obj.multiply(main.daneZKlawiatury(), main.daneZKlawiatury()));
+                break;
+            case 4:
+                System.out.println("Wykonujesz odejmowanie");
+                System.out.println(obj.subtract(main.daneZKlawiatury(), main.daneZKlawiatury()));
+                break;
+            case 5:
+                System.out.println("Wykonujesz potege");
+                System.out.println(obj.power(main.daneZKlawiatury(), main.daneZKlawiatury()));
+                break;
+            case 6:
+                System.out.println("Wykonujesz pierwiastek");
+                System.out.println(obj.sqrt(main.daneZKlawiatury()));
+                break;
+                default:
+                    System.out.println(" Nie ma takiej opcji ");
+        }
     }
+        public double daneZKlawiatury () {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Podaj  liczbę");
+            double a = sc.nextDouble();
+            return a;
+
+        }
+
 }
